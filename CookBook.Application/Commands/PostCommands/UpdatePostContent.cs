644da9b24e0350d638d130099f3ac02b02b4a267
Cezar Buna <cezar.buna@amdaris.com>
2022-05-13 15:29:38 +0300

    added UpdatePostContent command and command handler

---
+++ CookBook.Application/Commands/PostCommands/UpdatePostContent.cs
@@ -0,0 +1,16 @@
+﻿using CookBook.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Application.Commands.PostCommands
+{
+    public class UpdatePostContent : IRequest<Post>
+    {
+        public Guid PostId { get; set; }
+        public string NewPostContent { get; set; }
+    }
+}
