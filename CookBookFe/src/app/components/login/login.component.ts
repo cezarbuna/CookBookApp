3b6fde44de0ff9e1c32ce0bc5f2eb5399d6fe643
Cezar Buna <cezar.buna@amdaris.com>
2022-05-17 21:32:14 +0300

    added login UI for normal users

--- CookBookFe/src/app/components/login/login.component.ts
+++ CookBookFe/src/app/components/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {UserInterface} from "../../models/user-interface";
+import {Router} from "@angular/router";
+import {UsersService} from "../../services/users.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -10,10 +14,42 @@ export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
 
-  constructor() {
-  }
+  users: UserInterface[] = [];
+
+  constructor(private router: Router, private usersService: UsersService, private httpClient: HttpClient, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.usersService.getUsers().subscribe(res => {
+      this.users = res;
+      console.log(res);
+    })
+
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    })
+  }
+
+  login() {
+
+    this.usersService.getUsers().subscribe(res => {
+      //console.log("triggered");
+
+      const user = res.find((a: UserInterface) => {
+        return a.userName === this.loginForm.value.username && a.password === this.loginForm.value.password;
+      });
+
+      if(user){
+        alert("Login successful!");
+        console.log(user);
+        this.loginForm.reset();
+        this.router.navigate(['/home']);
+      }else {
+        alert("User not found!");
+      }
+    }, err => {
+      alert("Something went wrong!");
+    })
 
   }
 }
