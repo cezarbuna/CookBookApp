96f2507063194b1a99b2a1c0abcea62b1f8b1cfb
Cezar Buna <cezar.buna@amdaris.com>
2022-05-17 23:04:50 +0300

    added register and login for admins UI and switch buttons

--- CookBookFe/src/app/components/admin-login/admin-login.component.ts
+++ CookBookFe/src/app/components/admin-login/admin-login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {AdminInterface} from "../../models/admin-interface";
+import {AdminService} from "../../services/admin.service";
 
 @Component({
   selector: 'app-admin-login',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  admins: AdminInterface[] = [];
+
+  constructor(private router: Router, private adminService: AdminService, private httpClient: HttpClient, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.adminService.getAdmins().subscribe(res => {
+      this.admins = res;
+      console.log(res);
+    })
+
+    this.loginForm = this.formBuilder.group({
+      adminId: [''],
+      username: [''],
+      password: ['']
+    })
+  }
+
+  login() {
+
+    this.adminService.getAdmins().subscribe(res => {
+      //console.log("triggered");
+
+      const admin = res.find((a: AdminInterface) => {
+        return a.userName === this.loginForm.value.username && a.password === this.loginForm.value.password && a.adminId === this.loginForm.value.adminId;
+      });
+
+      if(admin){
+        alert("Login successful!");
+        console.log(admin);
+        this.loginForm.reset();
+        this.router.navigate(['/home']);
+      }else {
+        alert("Admin not found!");
+      }
+    }, err => {
+      alert("Something went wrong!");
+    })
+
   }
 
 }
--- CookBookFe/src/app/components/admin-register/admin-register.component.ts
+++ CookBookFe/src/app/components/admin-register/admin-register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {UserInterface} from "../../models/user-interface";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {UsersService} from "../../services/users.service";
+import {AdminInterface} from "../../models/admin-interface";
+import {AdminService} from "../../services/admin.service";
 
 @Component({
   selector: 'app-admin-register',
@@ -7,9 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminRegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  admins: AdminInterface[] = [];
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router, private adminService: AdminService) { }
 
   ngOnInit(): void {
+    this.adminService.getAdmins().subscribe(res => {
+      this.admins = res;
+      console.log(res);
+    })
+
+    this.registerForm = this.formBuilder.group({
+      adminId: [''],
+      userName: [''],
+      password: ['']
+    })
+
+  }
+
+  register() {
+    this.adminService.postAdmin(this.registerForm.value)
+      .subscribe(res => {
+        console.log(res);
+        this.router.navigate(['/home']);
+      })
+
+
   }
 
 }
