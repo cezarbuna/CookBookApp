0ba2305fb62418477ab27f908f88d3bdf8f70c56
Cezar Buna <cezar.buna@amdaris.com>
2022-05-21 20:52:01 +0300

    added new method in PostsController for updating all post properties + it's command and command handler

---
+++ CookBook.Application/CommandHandlers/PostCommandHandlers/UpdatePostHandler.cs
@@ -0,0 +1,37 @@
+﻿using CookBook.Application.Commands.PostCommands;
+using CookBook.Domain.IRepositories;
+using CookBook.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Application.CommandHandlers.PostCommandHandlers
+{
+    public class UpdatePostHandler : IRequestHandler<UpdatePost, Post>
+    {
+        private readonly IPostRepository repository;
+
+        public UpdatePostHandler(IPostRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public Task<Post> Handle(UpdatePost request, CancellationToken cancellationToken)
+        {
+            var updated = repository.GetEntityByID(request.PostId);
+
+            updated.Id = request.PostId;
+            updated.Title = request.Title;
+            updated.Content = request.Content;
+            updated.Category = request.Category;
+
+            repository.Update(updated);
+            repository.SaveChanges();
+
+            return Task.FromResult(updated);
+        }
+    }
+}
---
+++ CookBook.Application/Commands/PostCommands/UpdatePost.cs
@@ -0,0 +1,18 @@
+﻿using CookBook.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Application.Commands.PostCommands
+{
+    public class UpdatePost : IRequest<Post>
+    {
+        public Guid PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Category { get; set; }
+    }
+}
