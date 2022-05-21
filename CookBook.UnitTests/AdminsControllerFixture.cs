2a878a83635fe7e36d9bdd7ab8d7b8a04d6bce84
Cezar Buna <cezar.buna@amdaris.com>
2022-05-22 00:47:49 +0300

    added empty MS Test project with 4 empty test classes, one for each controller

---
+++ CookBook.UnitTests/AdminsControllerFixture.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.UnitTests
+{
+    [TestClass]
+    public class AdminsControllerFixture
+    {
+        private readonly Mock<IMediator> _mockMediator = new Mock<IMediator>();
+        private readonly Mock<IMapper> _mockMapper = new Mock<IMapper>();
+    }
+}
---
+++ CookBook.UnitTests/UsersControllersFixture.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.UnitTests
+{
+    [TestClass]
+    public class UsersControllersFixture
+    {
+        private readonly Mock<IMediator> _mockMediator = new Mock<IMediator>();
+        private readonly Mock<IMapper> _mockMapper = new Mock<IMapper>();
+
+
+    }
+}
