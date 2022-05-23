d4d208690fce846f9dbec6c0613db0182b4bc242
AAlexA300 <agoalex3@gmail.com>
2022-05-23 10:33:40 +0300

    added BAD POSTS navbar button and routing to it's component if the user is logged as an admin

---
+++ CookBookFe/src/app/components/badly-rated-posts/badly-rated-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-badly-rated-posts',
+  templateUrl: './badly-rated-posts.component.html',
+  styleUrls: ['./badly-rated-posts.component.css']
+})
+export class BadlyRatedPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
